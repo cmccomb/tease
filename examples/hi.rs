359990de7a18e3438ebfbbd233a47031a971efd8
Chris McComb <ccmcc2012@gmail.com>
2022-06-11 17:41:48 -0400

    More general

--- examples/hi.rs
+++ examples/hi.rs
@@ -1,8 +1,22 @@
+use tease::{run, train_linear_regression, Input};
+
 fn main() {
-    let model = tease::train_linear_regression();
-    tease::run(
+    let model = train_linear_regression();
+    run(
         move |x| model.predict(&x).unwrap(),
-        &[tease::Input::Number(0.3)],
+        &[
+            Input::Number(234.289),
+            Input::Number(235.6),
+            Input::Number(159.0),
+            Input::Number(107.608),
+            Input::Slider {
+                min: 1940.0,
+                max: 1970.0,
+                step: 0.33,
+                initial_value: 1947.0,
+            },
+            Input::Number(60.323),
+        ],
     )
     .expect("Could not run");
 }
