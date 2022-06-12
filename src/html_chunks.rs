03d36dd187df1f90d09c87ee4db037407fb7d0c8
Chris McComb <ccmcc2012@gmail.com>
2022-06-12 12:50:40 -0400

    New examples and more input types

---
+++ src/html_chunks.rs
@@ -0,0 +1,102 @@
+pub(crate) fn add_number(index: usize, initial_value: &f64) -> String {
+    format!("\
+    <label for=\"exampleInput{index}\" class=\"col-sm-2 col-form-label mt-3\"><i>x<sub>{index}</sub> = </i></label>\
+    <div class=\"col-sm-10 mt-3\">\
+        <input type=\"text\" class=\"form-control input\" id=\"exampleInput{index}\" name=\"x{index}\" aria-describedby=\"input6\" placeholder=\"x{index}\" value=\"{initial_value}\">\
+    </div>")
+}
+
+pub(crate) fn add_text(index: usize, initial_value: &str) -> String {
+    format!("\
+    <label for=\"exampleInput{index}\" class=\"col-sm-2 col-form-label mt-3\"><i>x<sub>{index}</sub> = </i></label>\
+    <div class=\"col-sm-10 mt-3\">\
+        <input type=\"text\" class=\"form-control input\" id=\"exampleInput{index}\" name=\"x{index}\" aria-describedby=\"input6\" placeholder=\"x{index}\" value=\"{initial_value}\">\
+    </div>")
+}
+
+pub(crate) fn add_slider(
+    index: usize,
+    initial_value: &f64,
+    max: &f64,
+    min: &f64,
+    step: &f64,
+) -> String {
+    format!("\
+    <label for=\"exampleInput{index}\" class=\"col-sm-2 col-form-label mt-3\"><i>x<sub>{index}</sub> = </i></label>\
+    <div class=\"col-sm-10 mt-3 form-group\" style=\"display: flex\">\
+        <input type=\"text\" class=\"form-control col-sm-2\" value=\"{initial_value}\" readonly>
+        <span class=\"col-sm-1\" ></span>
+        <input type=\"range\" class=\"form-control input col-sm-9\" min=\"{min}\" max=\"{max}\" step=\"{step}\" id=\"exampleInput{index}\" name=\"x{index}\" aria-describedby=\"input6\" placeholder=\"x{index}\" value=\"{initial_value}\" oninput=\"this.previousElementSibling.previousElementSibling.value = this.value\">\
+    </div>")
+}
+
+pub(crate) fn add_dropdown(
+    index: usize,
+    initial_value_index: &usize,
+    options: &Vec<f64>,
+) -> String {
+    let mut output = format!("\
+    <label for=\"exampleInput{index}\" class=\"col-sm-2 col-form-label mt-3\"><i>x<sub>{index}</sub> = </i></label>\
+    <div class=\"col-sm-10 mt-3\">\
+        <select class=\" form-control input\" aria-label=\"Default select example\">");
+
+    for (idx, option) in options.iter().enumerate() {
+        if idx == *initial_value_index {
+            output = format!(
+                "{}<option selected value=\"{option}\">{option}</option>",
+                output
+            )
+        } else {
+            output = format!("{}<option value=\"{option}\">{option}</option>", output)
+        }
+    }
+    output = format!("{}, </select></div>", output);
+    output
+}
+
+pub(crate) fn beginning() -> String {
+    "<html lang=\"en\">
+        <head>
+            <meta charset=\"utf-8\">
+            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+            <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">
+            <script type=\"text/javascript\">
+                function run_calculation() {
+                    var numbers = [];
+                    var classes = document.getElementsByClassName('input');
+                    Array.from(classes).forEach((x, i) => numbers.push(Number(x.value)));
+                    ipc.postMessage(numbers.toString());
+                }
+            </script>
+        </head>
+        <body>
+            <div class=\"container\">
+                <div class=\"row mt-3\">
+                    <div class=\"col text-center\">
+                        <form action=\"#\" method=\"POST\" onsubmit=\"run_calculation()\">
+                            <div class=\"form-group row\" id=\"input-group\">".to_string()
+}
+
+pub(crate) fn end() -> String {
+    "                        </div>
+
+                            <div class=\"form-group\" id=\"submit\">
+                                <button type=\"submit\" class=\"btn btn-primary\">Submit</button>
+                            </div>
+
+                            <div class=\"form-group row\" id=\"output-group\">
+                                <label for=\"output\" class=\"col-sm-2 col-form-label\"><i>y = </i></label>
+                                <div class=\"col-sm-10\">
+                                    <input type=\"text\" class=\"form-control\" id=\"output\" name=\"output\" aria-describedby=\"output\" readonly>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>
+            <script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\"></script>
+            <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>
+        </body>
+    </html>".to_string()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,9 @@ use wry::{
     webview::{WebView, WebViewAttributes, WebViewBuilder},
 };
 
+mod html_chunks;
+use html_chunks::{add_dropdown, add_number, add_slider, add_text, beginning, end};
+
 pub enum Input {
     Number(f64),
     Text(String),
@@ -28,9 +31,14 @@ pub enum Input {
         step: f64,
         initial_value: f64,
     },
+    Dropdown {
+        options: Vec<f64>,
+        initial_value: usize,
+    },
 }
 
-pub fn run<F>(predictor: F, inputs: &[Input]) -> wry::Result<()>
+pub fn run<F>(predictor: F, inputs: &[Input])
+//-> wry::Result<()>
 where
     F: 'static + Fn(DenseMatrix<f64>) -> Vec<f64>,
 {
@@ -45,13 +53,17 @@ where
             html,
             match input {
                 Input::Number(iv) => add_number(idx, iv),
-                Input::Text(_) => "".to_string(),
+                Input::Text(iv) => add_text(idx, iv),
                 Input::Slider {
                     min,
                     max,
                     step,
                     initial_value,
                 } => add_slider(idx, initial_value, max, min, step),
+                Input::Dropdown {
+                    initial_value,
+                    options,
+                } => add_dropdown(idx, initial_value, options),
             }
         );
     }
@@ -60,14 +72,16 @@ where
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("Tease")
-        .build(&event_loop)?;
+        .build(&event_loop)
+        .unwrap();
 
     let mut webview_settings = WebViewAttributes::default();
     webview_settings.devtools = true;
-    let mut webview_builder = WebViewBuilder::new(window)?;
+    let mut webview_builder = WebViewBuilder::new(window).unwrap();
     webview_builder.webview = webview_settings;
     let _webview = webview_builder
-        .with_html(html)?
+        .with_html(html)
+        .unwrap()
         .with_ipc_handler(move |_window: &Window, req: String| {
             let number_strings = req.split(",");
             let mut inputs = vec![0.0; 0];
@@ -90,7 +104,8 @@ where
                 })
                 .expect("TODO: panic message");
         })
-        .build()?;
+        .build()
+        .unwrap();
 
     WEBVIEW.with(|wv| {
         let mut hash = HashMap::new();
@@ -177,76 +192,4 @@ pub fn train_lasso_regression() -> Lasso<f64, DenseMatrix<f64>> {
     let lr = Lasso::fit(&x, &y, LassoParameters::default()).unwrap();
 
     lr
-}
-
-fn add_number(index: usize, initial_value: &f64) -> String {
-    format!("\
-    <label for=\"exampleInput{index}\" class=\"col-sm-2 col-form-label mt-3\"><i>x<sub>{index}</sub> = </i></label>\
-    <div class=\"col-sm-10 mt-3\">\
-        <input type=\"text\" class=\"form-control input\" id=\"exampleInput{index}\" name=\"x{index}\" aria-describedby=\"input6\" placeholder=\"x{index}\" value=\"{initial_value}\">\
-    </div>")
-}
-
-fn add_text(index: usize, initial_value: &str) -> String {
-    format!("\
-    <label for=\"exampleInput{index}\" class=\"col-sm-2 col-form-label mt-3\"><i>x<sub>{index}</sub> = </i></label>\
-    <div class=\"col-sm-10 mt-3\">\
-        <input type=\"text\" class=\"form-control input\" id=\"exampleInput{index}\" name=\"x{index}\" aria-describedby=\"input6\" placeholder=\"x{index}\" value=\"{initial_value}\">\
-    </div>")
-}
-
-fn add_slider(index: usize, initial_value: &f64, max: &f64, min: &f64, step: &f64) -> String {
-    format!("\
-    <label for=\"exampleInput{index}\" class=\"col-sm-2 col-form-label mt-3\"><i>x<sub>{index}</sub> = </i></label>\
-    <div class=\"col-sm-10 mt-3 form-group\" style=\"display: flex\">\
-            <input type=\"text\" class=\"form-control col-sm-2\" value=\"{initial_value}\" readonly>
-            <input type=\"range\" class=\"form-control input col-sm-10\" min=\"{min}\" max=\"{max}\" step=\"{step}\" id=\"exampleInput{index}\" name=\"x{index}\" aria-describedby=\"input6\" placeholder=\"x{index}\" value=\"{initial_value}\" oninput=\"this.previousElementSibling.value = this.value\">\
-    </div>")
-}
-
-fn beginning() -> String {
-    "<html lang=\"en\">
-        <head>
-            <meta charset=\"utf-8\">
-            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
-            <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">
-            <script type=\"text/javascript\">
-                function run_calculation() {
-                    var numbers = [];
-                    var classes = document.getElementsByClassName('input');
-                    Array.from(classes).forEach((x, i) => numbers.push(Number(x.value)));
-                    ipc.postMessage(numbers.toString());
-                }
-            </script>
-        </head>
-        <body>
-            <div class=\"container\">
-                <div class=\"row mt-3\">
-                    <div class=\"col text-center\">
-                        <form action=\"#\" method=\"POST\" onsubmit=\"run_calculation()\">
-                            <div class=\"form-group row\" id=\"input-group\">".to_string()
-}
-
-fn end() -> String {
-    "                        </div>         
-        
-                            <div class=\"form-group\" id=\"submit\">
-                                <button type=\"submit\" class=\"btn btn-primary\">Submit</button>
-                            </div>
-                        
-                            <div class=\"form-group row\" id=\"output-group\">
-                                <label for=\"output\" class=\"col-sm-2 col-form-label\"><i>y = </i></label>
-                                <div class=\"col-sm-10\">
-                                    <input type=\"text\" class=\"form-control\" id=\"output\" name=\"output\" aria-describedby=\"output\" readonly>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>  
-            </div>
-            <script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>
-            <script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\"></script>
-            <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>
-        </body>
-    </html>".to_string()
 }
